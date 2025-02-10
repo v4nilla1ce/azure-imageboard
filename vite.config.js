d097eadfd0c07c7a38cc343eaef2cb29a1d8f8da
v4nilla1ce <v4nilla@tuta.io>
2025-02-10 13:02:25 +0100

    fixed vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  base: './', // Use relative paths
-  build: {
-    outDir: 'dist',
-  },
 });
